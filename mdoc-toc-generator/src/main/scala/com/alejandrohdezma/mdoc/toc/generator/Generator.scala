ca5143377f80203ab45612e41a36f46c16d2871e
Alejandro Hernández <info@alejandrohdezma.com>
2020-10-06 10:02:31 +0200

    Avoid infix syntax with multiple parameters

--- mdoc-toc-generator/src/main/scala/com/alejandrohdezma/mdoc/toc/generator/Generator.scala
+++ mdoc-toc-generator/src/main/scala/com/alejandrohdezma/mdoc/toc/generator/Generator.scala
@@ -19,14 +19,18 @@ package com.alejandrohdezma.mdoc.toc.generator
 object Generator {
 
   def toc(text: String): String =
-    text.split('\n').filter(_.startsWith("##")).map {
-      case H2(header) => s"- [$header](#${link(header)})"
-      case H3(header) => s"  - [$header](#${link(header)})"
-      case H4(header) => s"    - [$header](#${link(header)})"
-      case H5(header) => s"      - [$header](#${link(header)})"
-      case H6(header) => s"        - [$header](#${link(header)})"
-      case H7(header) => s"          - [$header](#${link(header)})"
-    } mkString ("---\n\n", "\n", "\n")
+    text
+      .split('\n')
+      .filter(_.startsWith("##"))
+      .map {
+        case H2(header) => s"- [$header](#${link(header)})"
+        case H3(header) => s"  - [$header](#${link(header)})"
+        case H4(header) => s"    - [$header](#${link(header)})"
+        case H5(header) => s"      - [$header](#${link(header)})"
+        case H6(header) => s"        - [$header](#${link(header)})"
+        case H7(header) => s"          - [$header](#${link(header)})"
+      }
+      .mkString("---\n\n", "\n", "\n")
 
   def link(header: String): String =
     header
